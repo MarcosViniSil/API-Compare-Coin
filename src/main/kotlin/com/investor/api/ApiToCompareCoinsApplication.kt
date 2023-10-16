66f729df6a20e4a6fbe1f3b48a8281517bbce4cc
MarcosViniSil <Marcossilv203@gmail.com>
2023-10-16 11:27:40 -0300

    create project

---
+++ src/main/kotlin/com/investor/api/ApiToCompareCoinsApplication.kt
@@ -0,0 +1,11 @@
+package com.investor.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiToCompareCoinsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiToCompareCoinsApplication>(*args)
+}
