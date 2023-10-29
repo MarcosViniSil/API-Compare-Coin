fdbbd072ce5ffbb2dff350499dc9d5ff2a974ffc
MarcosViniSil <Marcossilv203@gmail.com>
2023-10-28 21:19:15 -0300

    correction cors

--- src/main/kotlin/com/investor/api/Application.kt
+++ src/main/kotlin/com/investor/api/Application.kt
@@ -1,13 +1,16 @@
 package com.investor.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.scheduling.annotation.EnableScheduling
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@OpenAPIDefinition(servers = [Server(url = "/", description = "Default Server URL")])
 class ApiToCompareCoinsApplication
 
 fun main(args: Array<String>) {
